7bcc3c50924b7ced4173f56522610453dc799144
MMADHOSIND <158587942+MMADHOSIND@users.noreply.github.com>
2024-04-12 14:35:03 +0330

    Add files via upload

---
+++ Project-master/Signin.php
@@ -0,0 +1,81 @@
+<?php
+    include("Includes\Include-top.php");
+?>
+.InputText{
+    width: 300px;
+    height: 30px;
+    border-radius: 12px;
+    padding-left: 7px;
+    padding-right: 7px;
+    margin-left: 30px;
+}
+    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <title>Document</title>
+   <body style="background-image: url(Images/art-7737624_1920.jpg);">
+</head>
+<body>
+    <table  style=" width: 100%;">
+        <tr>
+            <td>
+                <div class="main-top-div">
+        		    <div class="subdiv-left">
+		    	        <a href="index.php" class="underline" >Home</a>
+			            <a href="Games.php" class="underline" >Games</a>
+    			        <a href="Account.php" class="underline" >Acount</a>
+	    		        <a href="Wallet.php" class="underline" >Wallet</a>
+		            </div>
+		            <div class="subdiv">
+			            <div class="triangle-down"></div>
+    		        </div>
+	    	        <div class="subdiv-right">
+		    	        <a href="#" class="underline" style="background-color: orange; border-radius: 5px;" class="underline" >Signin/Login</a>
+    		        </div>
+	            </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div class="fdiv">
+                    <form action="Actions\actionSignin.php" method="POST" name="form">
+                        <!-- <div class="title">Sign in</div> -->
+                        <div>
+                            <h4 style="float: left;">Name</h4>
+                            <input type="text" name="Name" minlength="2" maxlength="20" required class="InputText">
+                            <br>
+                            <h4 style="float: left;">Username</h4>
+                            <input type="text" name="UserName"  maxlength="20" required class="InputText">                            
+                            <br>
+                            <h4 style="float: left;">Password</h4>
+                            <input type="password" name="Pass" minlength="8" maxlength="20" required class="InputText">
+                            <br>
+                            <h4 style="float: left;">Email</h4>
+                            <input type="Email" name="Email" class="InputText">
+                            <br>
+                            <h4 style="float: left;">Password</h4>
+                            <input type="text" name="GmailPassword" minlength="2" maxlength="20" required class="InputText">
+                        </div>
+                        <button type="submit" >Next Step</button>
+                    </form>
+                </div>            
+            </td>
+        </tr>
+    </table>
+
+
+
+
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
---
+++ Project-master/login.php
@@ -0,0 +1,51 @@
+<?php
+    include("Includes\Include-top.php");
+?>
+.InputText{
+    width: 300px;
+    height: 30px;
+    border-radius: 12px;
+    padding-left: 7px;
+    padding-right: 7px;
+    margin-left: 30px;
+}
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table  style=" width: 100%;">
+        <tr>
+            <td>
+                <div class="main-top-div">
+        		    <div class="subdiv-left">
+		    	        <a href="index.php" class="underline" >Home</a>
+			            <a href="Games.php" class="underline" >Games</a>
+    			        <a href="Account.php" class="underline" >Acount</a>
+	    		        <a href="Wallet.php" class="underline" >Wallet</a>
+		            </div>
+		            <div class="subdiv">
+			            <div class="triangle-down"></div>
+    		        </div>
+	    	        <div class="subdiv-right">
+		    	        <a href="#" class="underline" style="background-color: orange; border-radius: 5px;" class="underline" >Signin/Login</a>
+    		        </div>
+	            </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+            <form action="Actions\actionLogin.php" method="post" style="width: 49%;">
+                <h4 style="float: left;">username</h4><input type="text" name="username" class="InputText">
+                <br>
+                <h4 style="float: left;">password</h4><input type="password" name="password"  class="InputText">
+                <br>
+                <a href="Signin.php" style="text-decoration: none; color: orange; font-size: large;">Register</a>
+                <input type="submit">
+            </form>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
